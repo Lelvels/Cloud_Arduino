6b15e6055e36e61bbae466813fe87c8b915bde72
Lelvels <levels1912@gmail.com>
2022-04-11 11:28:06 +0700

    Code arduino for demo 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,82 @@
-#include <Arduino.h>
-#include <DHT.h>
-#include <ReportedData.h> 
-#include <DesiredData.h>
+#include<Arduino.h>
+#include<DHT.h>
+#include<ArduinoJson.h>
+#include<LiquidCrystal_I2C.h>
 
-#define DHTPIN 2
+#define DHTPIN 7
 #define DHTTYPE DHT22
 
-const char ARDUINO_ID[6] = "AR01";
-String send_msg = "";
-String receive_msg = "";
+const char ARDUINO_ID[10] = "AR01";
+String command;
+float desiredTemp;
+float desiredHumid;
+bool send_data;
+uint16_t last_sent = 0;
 
 DHT dht(DHTPIN, DHTTYPE);
-ReportedData reported(ARDUINO_ID);
-DesiredData desired(ARDUINO_ID);
+LiquidCrystal_I2C lcd(0x27, 20, 4);
 
-void setup() {
+void setup(){
   Serial.begin(9600);
   dht.begin();
-  while (!Serial) {
+  while (!Serial)
+  {
     ; // wait for serial port to connect. Needed for native USB
   }
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  desiredHumid = 0;
+  desiredTemp = 0;
+  lcd.begin();
+  lcd.backlight();
 }
 
-void loop() {
-  Serial.println("AR01");
-  //Reading data from sensor
-  float temp_Reported = dht.readTemperature();
-  float humid_Reported = dht.readHumidity();
-  receive_msg = "";
+void loop(){
+  StaticJsonDocument<200> doc;
+  if (Serial.available()) {
+    command = Serial.readStringUntil('\n');
+    command.trim();
 
-  //Sending sensor data to the rasberry
-  if (isnan(humid_Reported) || isnan(temp_Reported)) {
-    Serial.println(F("SENSOR_ERR"));
-  } else {
-    //SEND REPORTED DATA THEN RECEIVE SEND DATA
-    if(Serial.available() > 0){
-      receive_msg = Serial.readStringUntil('\n');
-      receive_msg.trim();
-      Serial.print("Arduino got: ");
-      Serial.println(receive_msg);
-      int check = receive_msg.equals("ACK");
-      Serial.print("Check got: ");
-      Serial.println(check);
-
-      if (!check) {
-        reported.setTemperature(temp_Reported);
-        reported.setHumidity(humid_Reported);
-        String jsonString = reported.toJSON();
-        Serial.println(jsonString);
-        Serial.println("STOP");
-      } else {     
-        Serial.println("Arduino is processing data");
-      }
+    DeserializationError error = deserializeJson(doc, command);
+    if(error){
+      desiredHumid = -1;
+      desiredTemp = -1;
+      return;
+    } else {
+      desiredTemp = doc["Temperature"];
+      desiredHumid = doc["Humidity"];
     }
+
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Id:");
+    lcd.setCursor(10,0);
+    lcd.print(ARDUINO_ID);
+
+    lcd.setCursor(0,1);
+    lcd.print("Temp:");
+    lcd.setCursor(10,1);
+    lcd.print(desiredTemp);
+
+    lcd.setCursor(0,2);
+    lcd.print("Humid:");
+    lcd.setCursor(10,2);
+    lcd.print(desiredHumid);
+  }
+
+  uint16_t current_sent = millis();
+  if(current_sent - last_sent > 10000){
+    float temp = dht.readTemperature();
+    float humid = dht.readHumidity();
+
+    if(isnan(temp) || isnan(humid)){
+      Serial.println("DHT_ERR");
+    } else {
+      doc["ArduinoId"] = ARDUINO_ID;
+      doc["Temperature"] = temp;
+      doc["Humidity"] = humid;
+      serializeJson(doc, Serial);
+    }
+    last_sent = current_sent;
   }
 }
